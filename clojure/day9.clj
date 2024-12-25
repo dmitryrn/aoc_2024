2843b7a6670564aca4146faee76865266a7a7311
Dmitrii Rog <dmitry.rog.jrn@gmail.com>
2024-12-25 09:29:24 +0100

    fix

--- clojure/day9.clj
+++ clojure/day9.clj
@@ -8,21 +8,21 @@
 
 (defn expand [layout]
   (flatten (let [enumerated (map-indexed (fn [i v] [(inc i) v]) layout)]
-    (map 
-      (fn [[i v]] (my-range i v))
-      enumerated)))
-  )
+            (map 
+              (fn [[i v]] (my-range i v))
+              enumerated))))
+  
 
 (defn compact [list]
   (let [
-    enumerated (map-indexed (fn [i v] [i v]) list)
-    [el-i el-v] (some (fn [[i, v]] (if (> v -1) [i v] nil)) (reverse enumerated))
-    emp-i (some (fn [[i v]] (if (= v -1) i nil)) enumerated)]
+        enumerated (map-indexed (fn [i v] [i v]) list)
+        [el-i el-v] (some (fn [[i, v]] (if (> v -1) [i v] nil)) (reverse enumerated))
+        emp-i (some (fn [[i v]] (if (= v -1) i nil)) enumerated)]
     (if (> emp-i el-i)
       list
       (let [
-        list (assoc (vec list) emp-i el-v)
-        list (assoc (vec list) el-i -1)]
+            list (assoc (vec list) emp-i el-v)
+            list (assoc (vec list) el-i -1)]
         (recur list)))))
 
 (defn checksum [list]
@@ -40,11 +40,11 @@
 (deftest test-compact1
   (testing "compact"
     (is (= 
-      (-> [1 2 3 4 5] expand compact)
-      [0 2 2 1 1 1 2 2 2 -1 -1 -1 -1 -1 -1]))
+         (-> [1 2 3 4 5] expand compact)
+         [0 2 2 1 1 1 2 2 2 -1 -1 -1 -1 -1 -1]))
     (is (= 
-      (-> [2 3 3 3 1 3 3 1 2 1 4 1 4 1 3 1 4 0 2] expand compact)
-      (parse "0099811188827773336446555566..............")))))
+         (-> [2 3 3 3 1 3 3 1 2 1 4 1 4 1 3 1 4 0 2] expand compact)
+         (parse "0099811188827773336446555566..............")))))
 
 (deftest test-spl-line
   (testing "spl-line"
